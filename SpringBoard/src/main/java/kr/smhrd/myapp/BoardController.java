4b1b1ad755fb73db6fd568a88e3615af7322bc21
vipstard <vipstar_d@naver.com>
2022-03-16 14:15:11 +0900

    22.03.16

--- SpringBoard/src/main/java/kr/smhrd/myapp/BoardController.java
+++ SpringBoard/src/main/java/kr/smhrd/myapp/BoardController.java
@@ -4,24 +4,27 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.smhrd.domain.Board;
+import kr.smhrd.mapper.BoardMapper;
 
 //@Controller : Spring MVC 프로젝트에서 컨트롤러 역할
 // 주로 View를 반환하기 위해 사용
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardMapper mapper;
+	
 	//게시판리스트 요청받는 메서드
 	//@RequestMapping : 특정 URI로 온 요청 처리 방식 정의
 	@RequestMapping("/boardList.do")
 	public String boardList(HttpServletRequest request) {
-		ArrayList<String> list = new ArrayList<String>();
-		
-		list.add("봄");
-		list.add("여름");
-		list.add("가을");
-		list.add("겨울");
+		ArrayList<Board> list = mapper.boardList();
+
 		
 		request.setAttribute("list", list);
 		
